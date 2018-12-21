d06bbcf50be3c239c5409086500c936fe2995140
Taras Chernysh <taraschernysh24@gmail.com>
2018-12-21 11:18:11 +0200

    Updates

---
+++ precache-manifest.ddd0f103f6cb380fc8aff954b0c9900d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "778944165491956db8cb",
+    "url": "/Tarassn/react-weathercheck-practice/static/css/main.2a15aafb.chunk.css"
+  },
+  {
+    "revision": "778944165491956db8cb",
+    "url": "/Tarassn/react-weathercheck-practice/static/js/main.77894416.chunk.js"
+  },
+  {
+    "revision": "e4e6c235e9094352c21c",
+    "url": "/Tarassn/react-weathercheck-practice/static/css/1.cba28dd2.chunk.css"
+  },
+  {
+    "revision": "e4e6c235e9094352c21c",
+    "url": "/Tarassn/react-weathercheck-practice/static/js/1.e4e6c235.chunk.js"
+  },
+  {
+    "revision": "8c513a0284f933750fa3",
+    "url": "/Tarassn/react-weathercheck-practice/static/js/runtime~main.8c513a02.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Tarassn/react-weathercheck-practice/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "164ff6be8854126a639ae1ae91a3669f",
+    "url": "/Tarassn/react-weathercheck-practice/index.html"
+  }
+];
