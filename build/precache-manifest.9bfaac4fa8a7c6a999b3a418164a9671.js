eb2db8e6c654b331ee4a1566d6a09211532d2bf4
Taras Chernysh <taraschernysh24@gmail.com>
2018-12-21 11:06:39 +0200

    0.1

---
+++ build/precache-manifest.9bfaac4fa8a7c6a999b3a418164a9671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46f75e87d8e406aafac9",
+    "url": "/static/js/main.46f75e87.chunk.js"
+  },
+  {
+    "revision": "e4e6c235e9094352c21c",
+    "url": "/static/js/1.e4e6c235.chunk.js"
+  },
+  {
+    "revision": "46f75e87d8e406aafac9",
+    "url": "/static/css/main.8755fe96.chunk.css"
+  },
+  {
+    "revision": "e4e6c235e9094352c21c",
+    "url": "/static/css/1.cba28dd2.chunk.css"
+  },
+  {
+    "revision": "083a3edbcb112bd4433d27d43288e0e4",
+    "url": "/index.html"
+  }
+];
