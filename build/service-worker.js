18982170c55b1448ae0f6f55696baf5b4950cc5e
Taras Chernysh <taraschernysh24@gmail.com>
2018-12-21 11:53:22 +0200

    0.1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bfaac4fa8a7c6a999b3a418164a9671.js"
+  "/react-weathercheck-practice/precache-manifest.34fa43b25227f17a51d35874e17433b8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-weathercheck-practice/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
